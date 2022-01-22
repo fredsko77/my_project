5522895c9b182739dfd1731afca50ba5c06b0355
frederickagathe <45557712+fredsko77@users.noreply.github.com>
2022-01-22 22:10:20 +0100

    Update settings.php

--- app/settings.php
+++ app/settings.php
@@ -27,14 +27,6 @@ return function (Container $container) {
                 'user' => 'root',
                 'pass' => '',
             ],
-            'mailer' => [
-                // use 'null' for the null adapter
-                'type' => 'smtp',
-                'host' => 'smtp.gmail.com',
-                'port' => '587',
-                'username' => 'testwamp08@gmail.com',
-                'password' => '1995Posse',
-            ],
         ];
     });
 };
